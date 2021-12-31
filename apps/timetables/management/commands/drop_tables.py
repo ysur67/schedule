183c308ee62e96382627c909a5acc6ae4f5e29df
ysur67 <rus.muratow@gmail.com>
2021-12-31 14:05:39 +0300

    feat: add drop_tables command

---
+++ apps/timetables/management/commands/drop_tables.py
@@ -0,0 +1,13 @@
+from typing import Any, Optional
+from django.core.management import BaseCommand
+from apps.timetables.models import Group, Classroom, Lesson, Subject, Teacher
+
+
+class Command(BaseCommand):
+    models = (
+        Group, Classroom, Lesson, Subject, Teacher
+    )
+
+    def handle(self, *args: Any, **options: Any) -> Optional[str]:
+        for model in self.models:
+            model.objects.all().delete()
