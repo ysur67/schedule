cf2a0362f735f88662e312f647a31d9422148394
ysur67 <rus.muratow@gmail.com>
2022-01-08 19:54:22 +0300

    feat: add educational level to status message

--- apps/feedback/bots/commands/utils.py
+++ apps/feedback/bots/commands/utils.py
@@ -4,6 +4,7 @@ from apps.feedback.models import Profile
 def build_status_message(profile: Profile) -> str:
     group = profile.get_group()
     result = f"Группа: {group.title}\n"
+    result += f"Уровень образования: {group.level.title.capitalize()}\n"
     send_notifications = "✅ Включены" if profile.send_notifications else "❌ Отключены"
     result += f"Уведомления: {send_notifications}\n"
     for account in profile.get_accounts_in_messengers():
