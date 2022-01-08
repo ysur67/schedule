2fd1d9cf8a850e92072409f3aa046f893b713000
ysur67 <rus.muratow@gmail.com>
2022-01-08 17:07:03 +0300

    ref: move status message to separate function

--- apps/feedback/bots/commands/get_current_status.py
+++ apps/feedback/bots/commands/get_current_status.py
@@ -1,5 +1,6 @@
 from os import sync
 from typing import Union
+from apps.feedback.bots.commands.utils import build_status_message
 from apps.feedback.models import Profile
 from apps.feedback.usecases.messenger import get_messenger_by_code
 from apps.feedback.usecases.profile import get_profile_by_messenger_and_account_id
@@ -16,16 +17,5 @@ class GetCurrentStatusCommand(BaseCommand):
     async def _vk_execute(self) -> Union[SingleMessage, MultipleMessages]:
         messenger = await sync_to_async(get_messenger_by_code)(self.type.value)
         profile = await sync_to_async(get_profile_by_messenger_and_account_id)(messenger, self.account_id)
-        msg = await self._build_message(profile)
+        msg = await sync_to_async(build_status_message)(profile)
         return SingleMessage(message=msg)
-
-    async def _build_message(self, profile: Profile) -> str:
-        group = await sync_to_async(profile.get_group)()
-        result = f"Группа: {group.title}\n"
-        send_notifications = "✅ Включены" if profile.send_notifications else "❌ Отключены"
-        result += f"Уведомления: {send_notifications}\n"
-        accounts_in_messengers = sync_to_async(list)(profile.get_accounts_in_messengers())
-        for account in await accounts_in_messengers:
-            messenger = await sync_to_async(account.get_messenger)()
-            result += f"Имеется аккаунт в {messenger.title}\n"
-        return result
---
+++ apps/feedback/bots/commands/utils.py
@@ -0,0 +1,12 @@
+from apps.feedback.models import Profile
+
+
+def build_status_message(profile: Profile) -> str:
+    group = profile.get_group()
+    result = f"Группа: {group.title}\n"
+    send_notifications = "✅ Включены" if profile.send_notifications else "❌ Отключены"
+    result += f"Уведомления: {send_notifications}\n"
+    for account in profile.get_accounts_in_messengers():
+        messenger = account.get_messenger()
+        result += f"Имеется аккаунт в {messenger.title}\n"
+    return result
