8c9a191286706dda439cb814b74acafe57e79c74
ysur67 <rus.muratow@gmail.com>
2022-01-18 20:45:21 +0300

    fix: print groups only if there is selected one

--- apps/feedback/bots/commands/utils.py
+++ apps/feedback/bots/commands/utils.py
@@ -7,8 +7,10 @@ from apps.timetables.models import Lesson
 
 def build_status_message(profile: Profile) -> str:
     group = profile.get_group()
-    result = f"Группа: {group.title}\n"
-    result += f"Уровень образования: {group.level.title.capitalize()}\n"
+    result = ""
+    if group:
+        result += f"Группа: {group.title}\n"
+        result += f"Уровень образования: {group.level.title.capitalize()}\n"
     send_notifications = "✅ Включены" if profile.send_notifications else "❌ Отключены"
     result += f"Уведомления: {send_notifications}\n"
     for account in profile.get_accounts_in_messengers():
--- apps/feedback/usecases/messenger.py
+++ apps/feedback/usecases/messenger.py
@@ -3,4 +3,7 @@ from apps.feedback.models import MessengerModel
 
 
 def get_messenger_by_code(code: str) -> Optional[MessengerModel]:
-    return MessengerModel.objects.filter(code__iexact=code).first()
+    result = MessengerModel.objects.filter(code__iexact=code).first()
+    if not result:
+        raise ValueError(f"There is no messenger with code: {code}")
+    return result
