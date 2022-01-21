c0ca5b075fe1f32ee2ef26c95b8b654e6ea7ca70
ysur67 <rus.muratow@gmail.com>
2022-01-22 01:34:45 +0300

    feat: add exchange settings model

--- apps/exchange/models.py
+++ apps/exchange/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class ExchangeSettings(models.Model):
+    is_parsing_enabled = models.BooleanField(
+        verbose_name="Парсинг активирован?",
+        default=True
+    )
+    lessons_start_date = models.DateField(
+        verbose_name="Дата начала для парсинга занятий",
+        default=now
+    )
+    lesson_end_date = models.DateField(
+        verbose_name="Конечная дата для парсинга занятий",
+        default=now
+    )
+
+    class Meta:
+        verbose_name = "Настройки парсинга"
+        verbose_name_plural = verbose_name
+
+    def __str__(self) -> str:
+        return self._meta.verbose_name
