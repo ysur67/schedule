d65c64ef93c73889fce646596db4a31610ffa689
ysur67 <rus.muratow@gmail.com>
2022-02-04 17:56:53 +0300

    add redirect to admin page

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/')),
     path('admin/', admin.site.urls),
 ]
