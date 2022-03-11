1817070a6e53e713e06509c23564cd29a90f5687
ysur67 <rus.muratow@gmail.com>
2022-03-12 00:19:45 +0300

    feat: create current user view

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.views import (
 )
 
 from rest_framework.routers import DefaultRouter
+from apps.feedback.views import CurrentUserView
 from apps.timetables.views import EducationalLevelViewSet
 
 
@@ -16,5 +17,6 @@ router.register('educational-levels', EducationalLevelViewSet)
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name="obtain_token"),
     path('token/refresh/', TokenRefreshView.as_view(), name="refresh_token"),
+    path('current-user/', CurrentUserView.as_view(), name="current_user"),
 ]
 urlpatterns += router.urls
