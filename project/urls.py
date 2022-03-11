59ad3710e7878caffea9c74a98ec099f421e3e60
ysur67 <rus.muratow@gmail.com>
2022-03-12 00:49:31 +0300

    add drf-yasg integration

--- project/urls.py
+++ project/urls.py
@@ -14,10 +14,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+...
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0),
+            name='schema-json'),
+    re_path(r'^swagger/$',
+            schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'),
+    re_path(r'^redoc/$',
+            schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'),
     path('', RedirectView.as_view(url='/admin/')),
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.main.urls'), name='main'),
