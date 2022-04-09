2c63c692cd8e7670ca4591fd43e53544d9cf1f92
ysur67 <rus.muratow@gmail.com>
2022-04-09 18:32:37 +0300

    fix: groups layout in telegram api

--- apps/feedback/bots/utils/keyboard/groups.py
+++ apps/feedback/bots/utils/keyboard/groups.py
@@ -52,7 +52,11 @@ class GroupsKeyboard(BaseKeyboard):
         return result
 
     def to_telegram_api(self) -> Union[ReplyKeyboardMarkup, List[InlineKeyboardMarkup]]:
-        if self.data.count() > VK_MAX_BUTTONS_IN_KEYBOARD:
+        count = self.data.count()
+        rows = count / self.OFFSET
+        if self.has_cancel_button:
+            rows += 1
+        if (count > VK_MAX_BUTTONS_IN_KEYBOARD) or (rows >= VK_MAX_ROWS_IN_KEYBOARD):
             self.is_inline = True
             return self._build_multiple_telegram_keyboards()
         return self._build_single_telegram_keyboard(self.data)
@@ -60,20 +64,30 @@ class GroupsKeyboard(BaseKeyboard):
     def _build_multiple_telegram_keyboards(self) -> List[InlineKeyboardMarkup]:
         paginator = Paginator(self.data.order_by("id"), self.ITEMS_PER_PAGE)
         result: List[InlineKeyboardMarkup] = []
+        self.has_cancel_button = False
         for index in paginator.page_range:
             page = paginator.page(index)
             result.append(
-                self._build_single_telegram_keyboard(page.object_list))
+                self._build_single_telegram_keyboard(page.object_list)
+            )
+            if index == paginator.num_pages - 1:
+                self.has_cancel_button = True
         return result
 
     def _build_single_telegram_keyboard(self, groups: QuerySet[Group]) -> Union[ReplyKeyboardMarkup, InlineKeyboardMarkup]:
         result = InlineKeyboardMarkup() if self.is_inline else ReplyKeyboardMarkup(
             resize_keyboard=True)
+        result.row_width = self.OFFSET
         for index, value in enumerate(groups):
             title = value.title
-            button = InlineKeyboardButton(
+            cancel_button = InlineKeyboardButton(
                 title, callback_data=title) if self.is_inline else KeyboardButton(title)
-            result.add(button)
-            if ((index + 1) % self.OFFSET == 0) and not self._is_last(index, groups):
+            result.insert(cancel_button)
+            if (((index + 1) % self.OFFSET) == 0) and not self._is_last(index, groups):
                 result.row()
+        if self.has_cancel_button:
+            cancel_button = InlineKeyboardButton(
+                'Главное меню', callback_data='Главное меню'
+            ) if self.is_inline else KeyboardButton('Главное меню')
+            result.add(cancel_button)
         return result
