53edca8afcaae813b17f33ddc36afd335f59b3cf
ysur67 <rus.muratow@gmail.com>
2022-06-05 17:53:51 +0300

    groups action

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
-
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView, TokenVerifyView)
+
 from apps.feedback.views import CurrentUserView
 from apps.timetables.views import EducationalLevelViewSet
-
 
 router = DefaultRouter()
 router.register('educational-levels', EducationalLevelViewSet)
@@ -17,6 +13,7 @@ router.register('educational-levels', EducationalLevelViewSet)
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name="obtain_token"),
     path('token/refresh/', TokenRefreshView.as_view(), name="refresh_token"),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('current-user/', CurrentUserView.as_view(), name="current_user"),
 ]
 urlpatterns += router.urls
--- project/settings.py
+++ project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'corsheaders',
 
     'apps.main',
     'apps.timetables',
@@ -54,11 +55,16 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'project.urls'
