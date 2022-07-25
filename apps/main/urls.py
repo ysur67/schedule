066b27d6939a744d483d7f1a3ba684c9ea17c9d1
ysur67 <rus.muratow@gmail.com>
2022-07-26 01:33:44 +0300

    remove redundant view

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -2,11 +2,8 @@ from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView, TokenVerifyView)
 
-from apps.feedback.views import CurrentUserView
-
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name="obtain_token"),
     path('token/refresh/', TokenRefreshView.as_view(), name="refresh_token"),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('current-user/', CurrentUserView.as_view(), name="current_user"),
 ]
